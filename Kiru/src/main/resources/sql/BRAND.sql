7cd2e93929cc7343b0a2e69c4be824582c267cd6
SCIT <SCIT@LAPTOP-7SOI2PVT>
2019-10-15 15:19:13 +0900

    Kiru 191015 15:19

---
+++ Kiru/src/main/resources/sql/BRAND.sql
@@ -0,0 +1,155 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 수요일-10월-09-2019   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table BRAND
+--------------------------------------------------------
+
+  CREATE TABLE "HR"."BRAND" 
+   (	"BRAND_NAME" VARCHAR2(200 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into HR.BRAND
+SET DEFINE OFF;
+Insert into HR.BRAND (BRAND_NAME) values ('BRAND_TEST');
+Insert into HR.BRAND (BRAND_NAME) values ('가먼트레이블');
+Insert into HR.BRAND (BRAND_NAME) values ('갭');
+Insert into HR.BRAND (BRAND_NAME) values ('골든구스');
+Insert into HR.BRAND (BRAND_NAME) values ('구찌');
+Insert into HR.BRAND (BRAND_NAME) values ('그라미치');
+Insert into HR.BRAND (BRAND_NAME) values ('꼬르넬리아니');
+Insert into HR.BRAND (BRAND_NAME) values ('꼼데가르송');
+Insert into HR.BRAND (BRAND_NAME) values ('나이키');
+Insert into HR.BRAND (BRAND_NAME) values ('낫포너드');
+Insert into HR.BRAND (BRAND_NAME) values ('노베스타');
+Insert into HR.BRAND (BRAND_NAME) values ('닐바렛');
+Insert into HR.BRAND (BRAND_NAME) values ('다이클레즈');
+Insert into HR.BRAND (BRAND_NAME) values ('닥스');
+Insert into HR.BRAND (BRAND_NAME) values ('닥터마틴');
+Insert into HR.BRAND (BRAND_NAME) values ('더지지');
+Insert into HR.BRAND (BRAND_NAME) values ('던스트');
+Insert into HR.BRAND (BRAND_NAME) values ('돈덥');
+Insert into HR.BRAND (BRAND_NAME) values ('돌체앤가바나');
+Insert into HR.BRAND (BRAND_NAME) values ('돔바');
+Insert into HR.BRAND (BRAND_NAME) values ('디그레');
+Insert into HR.BRAND (BRAND_NAME) values ('디아도라');
+Insert into HR.BRAND (BRAND_NAME) values ('디올');
+Insert into HR.BRAND (BRAND_NAME) values ('디프리크');
+Insert into HR.BRAND (BRAND_NAME) values ('딘스');
+Insert into HR.BRAND (BRAND_NAME) values ('딸리아또레');
+Insert into HR.BRAND (BRAND_NAME) values ('라르디니');
+Insert into HR.BRAND (BRAND_NAME) values ('라이더커뮨');
+Insert into HR.BRAND (BRAND_NAME) values ('라이풀');
+Insert into HR.BRAND (BRAND_NAME) values ('라코스테');
+Insert into HR.BRAND (BRAND_NAME) values ('락포트');
+Insert into HR.BRAND (BRAND_NAME) values ('랑데부');
+Insert into HR.BRAND (BRAND_NAME) values ('러기드하우스');
+Insert into HR.BRAND (BRAND_NAME) values ('레드윙');
+Insert into HR.BRAND (BRAND_NAME) values ('로스코');
+Insert into HR.BRAND (BRAND_NAME) values ('리우옴므');
+Insert into HR.BRAND (BRAND_NAME) values ('마르셀로불론');
+Insert into HR.BRAND (BRAND_NAME) values ('마시모알바');
+Insert into HR.BRAND (BRAND_NAME) values ('마에스트로');
+Insert into HR.BRAND (BRAND_NAME) values ('마이애미프로젝트');
+Insert into HR.BRAND (BRAND_NAME) values ('마틴로즈');
+Insert into HR.BRAND (BRAND_NAME) values ('매스노운');
+Insert into HR.BRAND (BRAND_NAME) values ('메종마르지엘라');
+Insert into HR.BRAND (BRAND_NAME) values ('메종키츠네');
+Insert into HR.BRAND (BRAND_NAME) values ('모아');
+Insert into HR.BRAND (BRAND_NAME) values ('뮌');
+Insert into HR.BRAND (BRAND_NAME) values ('바스');
+Insert into HR.BRAND (BRAND_NAME) values ('반스');
+Insert into HR.BRAND (BRAND_NAME) values ('발렌시아가');
+Insert into HR.BRAND (BRAND_NAME) values ('발렌티노');
+Insert into HR.BRAND (BRAND_NAME) values ('발루트');
+Insert into HR.BRAND (BRAND_NAME) values ('버버리');
+Insert into HR.BRAND (BRAND_NAME) values ('버켄스탁');
+Insert into HR.BRAND (BRAND_NAME) values ('베트멍');
+Insert into HR.BRAND (BRAND_NAME) values ('벤데이비스');
+Insert into HR.BRAND (BRAND_NAME) values ('부테로');
+Insert into HR.BRAND (BRAND_NAME) values ('브루넬로쿠치넬리');
+Insert into HR.BRAND (BRAND_NAME) values ('브아빗');
+Insert into HR.BRAND (BRAND_NAME) values ('빈스');
+Insert into HR.BRAND (BRAND_NAME) values ('산토니');
+Insert into HR.BRAND (BRAND_NAME) values ('살바토르페레가모');
+Insert into HR.BRAND (BRAND_NAME) values ('서플러스워드로브');
+Insert into HR.BRAND (BRAND_NAME) values ('슈프림');
+Insert into HR.BRAND (BRAND_NAME) values ('스탠레이');
+Insert into HR.BRAND (BRAND_NAME) values ('스톤아일랜드');
+Insert into HR.BRAND (BRAND_NAME) values ('스티그마');
+Insert into HR.BRAND (BRAND_NAME) values ('쓰래셔');
+Insert into HR.BRAND (BRAND_NAME) values ('씨피컴퍼니');
+Insert into HR.BRAND (BRAND_NAME) values ('아몬무브먼트');
+Insert into HR.BRAND (BRAND_NAME) values ('아미');
+Insert into HR.BRAND (BRAND_NAME) values ('아미리');
+Insert into HR.BRAND (BRAND_NAME) values ('아스페시');
+Insert into HR.BRAND (BRAND_NAME) values ('아웃스탠딩');
+Insert into HR.BRAND (BRAND_NAME) values ('아워레가시');
+Insert into HR.BRAND (BRAND_NAME) values ('아크네스튜디오');
+Insert into HR.BRAND (BRAND_NAME) values ('악셀아리가토');
+Insert into HR.BRAND (BRAND_NAME) values ('알두까다오스타');
+Insert into HR.BRAND (BRAND_NAME) values ('알레그리');
+Insert into HR.BRAND (BRAND_NAME) values ('알렉산더맥퀸');
+Insert into HR.BRAND (BRAND_NAME) values ('알파인더스트리');
+Insert into HR.BRAND (BRAND_NAME) values ('에스피오나지');
+Insert into HR.BRAND (BRAND_NAME) values ('에이글로우');
+Insert into HR.BRAND (BRAND_NAME) values ('에이지');
+Insert into HR.BRAND (BRAND_NAME) values ('엘엠씨');
+Insert into HR.BRAND (BRAND_NAME) values ('엠에스지엠');
+Insert into HR.BRAND (BRAND_NAME) values ('오니츠카타이거');
+Insert into HR.BRAND (BRAND_NAME) values ('오베이');
+Insert into HR.BRAND (BRAND_NAME) values ('오에이엠씨');
+Insert into HR.BRAND (BRAND_NAME) values ('오프화이트');
+Insert into HR.BRAND (BRAND_NAME) values ('와이쓰리');
+Insert into HR.BRAND (BRAND_NAME) values ('와이엠씨엘케이와이');
+Insert into HR.BRAND (BRAND_NAME) values ('유니폼브릿지');
+Insert into HR.BRAND (BRAND_NAME) values ('이티스');
+Insert into HR.BRAND (BRAND_NAME) values ('인로우스');
+Insert into HR.BRAND (BRAND_NAME) values ('인코텍스');
+Insert into HR.BRAND (BRAND_NAME) values ('일꼬르소');
+Insert into HR.BRAND (BRAND_NAME) values ('제냐');
+Insert into HR.BRAND (BRAND_NAME) values ('제이리움');
+Insert into HR.BRAND (BRAND_NAME) values ('주앙옴므');
+Insert into HR.BRAND (BRAND_NAME) values ('질스튜어트뉴욕');
+Insert into HR.BRAND (BRAND_NAME) values ('챔피언');
+Insert into HR.BRAND (BRAND_NAME) values ('처치스');
+Insert into HR.BRAND (BRAND_NAME) values ('츄바스코');
+Insert into HR.BRAND (BRAND_NAME) values ('칼하트');
+Insert into HR.BRAND (BRAND_NAME) values ('커먼프로젝트');
+Insert into HR.BRAND (BRAND_NAME) values ('컨버스');
+Insert into HR.BRAND (BRAND_NAME) values ('콘보이');
+Insert into HR.BRAND (BRAND_NAME) values ('콜한');
+Insert into HR.BRAND (BRAND_NAME) values ('콰시');
+Insert into HR.BRAND (BRAND_NAME) values ('크리스크리스티');
+Insert into HR.BRAND (BRAND_NAME) values ('킬리자로');
+Insert into HR.BRAND (BRAND_NAME) values ('탠디');
+Insert into HR.BRAND (BRAND_NAME) values ('텐모어');
+Insert into HR.BRAND (BRAND_NAME) values ('톰브라운');
+Insert into HR.BRAND (BRAND_NAME) values ('톰포드');
+Insert into HR.BRAND (BRAND_NAME) values ('티엔지티');
+Insert into HR.BRAND (BRAND_NAME) values ('팔칠엠엠서울');
+Insert into HR.BRAND (BRAND_NAME) values ('폴로랄프로렌');
+Insert into HR.BRAND (BRAND_NAME) values ('프라다');
+Insert into HR.BRAND (BRAND_NAME) values ('해지스');
+--------------------------------------------------------
+--  DDL for Index SYS_C007092
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "HR"."SYS_C007092" ON "HR"."BRAND" ("BRAND_NAME") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  Constraints for Table BRAND
+--------------------------------------------------------
+
+  ALTER TABLE "HR"."BRAND" MODIFY ("BRAND_NAME" NOT NULL ENABLE);
+  ALTER TABLE "HR"."BRAND" ADD PRIMARY KEY ("BRAND_NAME")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS"  ENABLE;
---
+++ Kiru/src/main/resources/sql/RECOMMEDDRESS.sql
@@ -0,0 +1,68 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 수요일-10월-09-2019   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table RECOMMEDDRESS
+--------------------------------------------------------
+
+  CREATE TABLE "HR"."RECOMMEDDRESS" 
+   (	"RECOMMED_NUM" VARCHAR2(20 BYTE), 
+	"GENRE_NAME" VARCHAR2(20 BYTE), 
+	"RECOMMED_PHOTO_NAME" VARCHAR2(200 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into HR.RECOMMEDDRESS
+SET DEFINE OFF;
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('4','classic','/sesoc/resources/img/fashion/classic/01.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('5','classic','/sesoc/resources/img/fashion/classic/02.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('6','classic','/sesoc/resources/img/fashion/classic/03.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('7','classic','/sesoc/resources/img/fashion/classic/04.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('8','classic','/sesoc/resources/img/fashion/classic/05.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('9','classiccasual','/sesoc/resources/img/fashion/classiccasual/01.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('10','classiccasual','/sesoc/resources/img/fashion/classiccasual/02.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('11','classiccasual','/sesoc/resources/img/fashion/classiccasual/03.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('12','classiccasual','/sesoc/resources/img/fashion/classiccasual/04.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('13','classiccasual','/sesoc/resources/img/fashion/classiccasual/05.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('14','minimal','/sesoc/resources/img/fashion/minimal/01.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('15','minimal','/sesoc/resources/img/fashion/minimal/02.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('16','minimal','/sesoc/resources/img/fashion/minimal/03.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('17','minimal','/sesoc/resources/img/fashion/minimal/04.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('18','minimal','/sesoc/resources/img/fashion/minimal/05.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('19','street','/sesoc/resources/img/fashion/street/01.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('20','street','/sesoc/resources/img/fashion/street/02.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('21','street','/sesoc/resources/img/fashion/street/03.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('22','street','/sesoc/resources/img/fashion/street/04.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('23','street','/sesoc/resources/img/fashion/street/05.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('24','amecaji','/sesoc/resources/img/fashion/amecaji/01.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('25','amecaji','/sesoc/resources/img/fashion/amecaji/02.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('26','amecaji','/sesoc/resources/img/fashion/amecaji/03.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('27','amecaji','/sesoc/resources/img/fashion/amecaji/04.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('28','amecaji','/sesoc/resources/img/fashion/amecaji/05.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('29','workwear','/sesoc/resources/img/fashion/workwear/01.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('30','workwear','/sesoc/resources/img/fashion/workwear/02.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('31','workwear','/sesoc/resources/img/fashion/workwear/03.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('32','workwear','/sesoc/resources/img/fashion/workwear/04.jpg');
+Insert into HR.RECOMMEDDRESS (RECOMMED_NUM,GENRE_NAME,RECOMMED_PHOTO_NAME) values ('33','workwear','/sesoc/resources/img/fashion/workwear/05.jpg');
+--------------------------------------------------------
+--  DDL for Index SYS_C007090
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "HR"."SYS_C007090" ON "HR"."RECOMMEDDRESS" ("RECOMMED_NUM") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  Constraints for Table RECOMMEDDRESS
+--------------------------------------------------------
+
+  ALTER TABLE "HR"."RECOMMEDDRESS" ADD PRIMARY KEY ("RECOMMED_NUM")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS"  ENABLE;
+  ALTER TABLE "HR"."RECOMMEDDRESS" MODIFY ("RECOMMED_PHOTO_NAME" NOT NULL ENABLE);
+  ALTER TABLE "HR"."RECOMMEDDRESS" MODIFY ("GENRE_NAME" NOT NULL ENABLE);
